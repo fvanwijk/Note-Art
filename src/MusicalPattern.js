34ff1356a0a2cad2fc66fa7d644319c831929f71
Seanitzel <seandvir12@gmail.com>
2018-11-22 00:32:34 +0200

    Note and Chord class now accept Object in constructor that holds the attributes for the instance

--- src/MusicalPattern.js
+++ src/MusicalPattern.js
@@ -1,4 +1,5 @@
 import {Note, piano} from '.'
+
 /**
  * Represents a musical pattern.
  */
@@ -9,32 +10,35 @@ export class MusicalPattern {
      *@constructor
      */
     constructor(tonic, pattern) {
-        if (tonic instanceof Note)
+        if (tonic instanceof Note) {
             this.tonic = tonic
-        else
-            this.tonic = new Note(tonic)
+        } else {
+            this.tonic = new Note({note: tonic})
+        }
         this.pattern = pattern
-        // this.notes = [this.tonic]
-        this._notes = []
+        this._notes  = []
         for (let j of pattern)
             this._notes.push(this.tonic.getInterval(j))
         this._notes.push(this.tonic.getInterval(12))
     }
+
     /**
      * returns array that contains all the notes in the pattern.
      */
     get notes() {
         return this._notes
     }
+
     /**
      * returns a string of all the notes in the pattern
      */
     toString() {
-        let str = ""
-        for (let i = 0; i < this.notes.length - 1; ++i) str += this.notes[i] + ", "
+        let str = ''
+        for (let i = 0; i < this.notes.length - 1; ++i) str += this.notes[i] + ', '
         str += this.notes[this.notes.length - 1]
         return str
     }
+
     /**
      * returns the interval inside the pattern
      * for example - if the pattern is C Major scale,
@@ -47,6 +51,7 @@ export class MusicalPattern {
             counter += this.pattern[i]
         return this.tonic.getInterval(counter)
     }
+
     /**
      * load all the sound files of the notes in the pattern.
      */
@@ -54,6 +59,7 @@ export class MusicalPattern {
         for (let n of this.notes)
             n.loadSound()
     }
+
     /**
      * play all the notes in the pattern one after the other.
      */
